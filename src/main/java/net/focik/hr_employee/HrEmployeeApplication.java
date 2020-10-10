ef62760d6ffb59b0b868db2b12279c43672208cc
Tomasz Krutowski <tkrutowski@gmail.com>
2020-10-10 23:33:48 +0200

    create  packages

---
+++ src/main/java/net/focik/hr_employee/HrEmployeeApplication.java
@@ -0,0 +1,13 @@
+package net.focik.hr_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/focik/hr_employee/HrEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.focik.hr_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
